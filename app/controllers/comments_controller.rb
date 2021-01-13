def19a86e44bf5977e80370f18f916089345f18e
naoki-nakajima <vsme@i.softbank.jp>
2021-01-13 20:44:17 +0900

    コメントコントローラー、createメソッドの実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to product_comments
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id])
+  end
+
 end
