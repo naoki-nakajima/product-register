97b89b1b8d9769914b8fdf0e565bfceb934db737
naoki-nakajima <vsme@i.softbank.jp>
2021-01-13 20:31:54 +0900

    コメントモデルのmigration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_034544) do
+ActiveRecord::Schema.define(version: 2021_01_13_112900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "product_id"
