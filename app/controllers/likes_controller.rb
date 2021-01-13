3158b29a96bdb3ed07a2ce0db207a7c7c808f742
naoki-nakajima <vsme@i.softbank.jp>
2021-01-13 20:29:38 +0900

    いいね機能の実装、コメントモデルの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,13 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, product_id: params[:product_id])
     @likes = Like.where(product_id: params[:product_id])
-    @product = Product.find8params[:product_id]
+    @product = Product.find(params[:product_id])
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, product_id: params[:product_id])
-    like.destroy
+    @like = Like.find_by(user_id: current_user.id, product_id: params[:product_id])
+    @like.destroy
     @likes = Like.where(product_id: params[:product_id])
-    @product = Product.find8params[:product_id]
+    @product = Product.find(params[:product_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :users, only: [:new, :edit, :update, :destroy]
-  resources :products, only: [:index, :new, :create, :edit, :update, :show, :destroy] do
+  resources :products do
     resources :likes, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
