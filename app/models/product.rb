b392e6c645c2148a37856fe20de338a2ce1995e0
naoki-nakajima <vsme@i.softbank.jp>
2021-01-13 20:36:50 +0900

    コメントモデルのアソシエーションの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :comments
   has_many :likes, dependent: :destroy
   acts_as_taggable
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :users, only: [:new, :edit, :update, :destroy]
   resources :products do
+    resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
