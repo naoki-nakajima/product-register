98820ba052c1e2cc37b23704650ab2f538809353
naoki-nakajima <vsme@i.softbank.jp>
2021-01-13 12:38:39 +0900

    likeモデルの実装 2 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :likes, dependent: :destroy
   acts_as_taggable
 
   validates :image, presence: true
